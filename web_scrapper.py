133ec0842c6d92db6e4a5aca2560d65fdf73df6e
AhmadAlnimrat <159529748+AhmadAlnimrat@users.noreply.github.com>
2024-02-15 16:19:52 +0300

    Update web_scrapper.py

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,7 +1,9 @@
+#importing require libraries
 from bs4 import BeautifulSoup
 import requests
 
 
+#Adding a username,password & the url
 username, password = 'USERNAME', 'PASSWORD'
 url  = "https://sandbox.oxylabs.io/products"
 payload = {
@@ -9,24 +11,27 @@ payload = {
     'render': 'html',
     'url': url,
 }
+
+
+#make a response by a request with a status code
 response = requests.post(
     'https://realtime.oxylabs.io/v1/queries',
     auth=(username, password),
     json=payload,
 )
-print(response.status_code)
 
-
+#scrapping content and having output as html
 content = response.json()["results"][0]["content"]
 soup = BeautifulSoup(content, "html.parser")
 
-
+#making a data list
 data = []
 for elem in soup.find_all("div", {"class": "product-card"}):
     title = elem.find('h4', {"class": "title"}).get_text(strip=True)
     price = elem.find('div', {"class": "price-wrapper"}).get_text(strip=True)
 
 
+    #filling the data
     availability = elem.find('p', {"class": ["in-stock", "out-of-stock"]}).get_text(strip=True)
     data.append({
         "title": title,
